12764bcd2604fc16ca2fa999838c80af5944cc6d
Dragaffa <noa.affagard@gmail.com>
2021-12-18 22:13:23 +0100

    6

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,9 +1,6 @@
 
 let timeline;
 class Tableau1 extends Phaser.Scene {
-    /**
-     * Précharge les assets
-     */
     preload() {
 
 
@@ -15,15 +12,11 @@ class Tableau1 extends Phaser.Scene {
         this.load.image('ville2', 'assets/ville2.png');
         this.load.image('ville3', 'assets/ville3.png');
 
-        this.load.image('avion', 'assets/plaine/avion_.png');
+        this.load.image('avion', 'assets/avion_1/avion.png');
         for(let j=1;j<=3;j++) {
-            this.load.image('avion' + j, 'assets/plaine/avion_' + j + '.png');
+            this.load.image('avion'+j, 'assets/avion_1/avion'+j+'.png');
         }
 
-        //au lieu d'écrire 5 lignes quasi identiques, on charge l'herbe avec une boucle
-        // ALGO : ceci est une boucle
-        //for (let i = 1; i <= 5; i++) {
-        //this.load.image('g-grass-' + i, 'assets/level/ground/g-grass-' + i + '.png');
     }
 
 
@@ -62,18 +55,16 @@ class Tableau1 extends Phaser.Scene {
         ville1.setScale(0.457)
 
         //ANIMATION
+
             this.avion = this.add.sprite(580, 0, 'avion').setOrigin(0,0);
             this.anims.create({
                 key: 'avion',
-                frames: this.getFrames('avion_',3),
+                frames: this.getFrames('avion',3),
                 frameRate: 16,
-                repeat: -1
-        });
-        avion.setScale(0.5)
+                repeat: 0
+            });
+        //this.avion.setScale(0.5)
 
-        //filtres
-
-        //gestion du parallaxe
         /**
          * Vitesse de déplacement du décor
          * @type {number}
@@ -81,19 +72,9 @@ class Tableau1 extends Phaser.Scene {
         this.speed = 0;
         //initialise ce qui se passe avec le clavier
         this.initKeyboard();
-        // Définit l'espace de déplacement de la caméra
-        this.cameras.main.setBounds(0, 0, 1073, 540);
-        //définit à quelles vitesse se déplacent nos différents plans
-        bgAnimationA.scrollFactorX = 0;
-        this.bg2Container.scrollFactorX = 10;
-        this.bg1Container.scrollFactorX = 10;
-        this.groundContainer.scrollFactorX = 10;
     }
 
-    /**
-     * Définit ce qui se passe quand on appuie ou relache une touche du clavier
-     * ALGO : ceci est une fonction ou méthode
-     */
+
     initKeyboard() {
         let me = this;
         this.input.keyboard.on('keydown', function (kevent) {
@@ -123,8 +104,7 @@ class Tableau1 extends Phaser.Scene {
         //déplacement de la caméra
         this.cameras.main.scrollX += this.speed; // on aurait pu écrire : this.cameras.main.scrollX= this.cameras.main.scrollX + this.speed;
 
-        //petit effet de vibrance sur le filtre film au tout premier plan
-        this.filterFilm.setAlpha(Phaser.Math.Between(95, 100) / 100)
+
     }
 
 }
